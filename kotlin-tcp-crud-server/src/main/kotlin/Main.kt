347c3c6028f4cb374041c2ef0f0f300daf90025f
Jordan <jordanduzabonfim@gmail.com>
2026-05-05 09:49:29 -0300

    server simple TCP connection

--- kotlin-tcp-crud-server/src/main/kotlin/Main.kt
+++ kotlin-tcp-crud-server/src/main/kotlin/Main.kt
@@ -1,16 +1,53 @@
 package bonfim.jordan
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+import kotlin.concurrent.thread
+
+fun main() {
+    val SERVER_PORT = 8080
+    val server = ServerSocket(SERVER_PORT)
+
+    println("Servidor TCP online na porta $SERVER_PORT.")
+    println("Aguardando conexões de entrada")
+
+    while (true) {
+        // Bloqueia até que um cliente estabeleça conexão
+        val clientSocket = server.accept()
+        println("Nova conexão estabelecida: ${clientSocket.inetAddress.hostAddress}")
+
+        // Inicia uma thread dedicada para não bloquear o loop principal
+        thread {
+            try {
+                val input = BufferedReader(InputStreamReader(clientSocket.inputStream))
+                val output = PrintWriter(clientSocket.outputStream, true)
+
+                // Mensagem de boas-vindas ao cliente
+                output.println("Conexão estabelecida")
+
+                var message: String?
+                // Lê as mensagens do cliente continuamente
+                while (input.readLine().also { message = it } != null) {
+                    println("[CLIENTE ${clientSocket.inetAddress.hostAddress}] $message")
+
+
+                    if (message?.trim( )?.lowercase() == "exit") {
+                        output.println("Encerrando conexão")
+                        break
+                    } else {
+                        output.println("Servidor recebeu: $message")
+                    }
+                }
+            } catch (e: Exception) {
+                println("[ERRO] Falha na comunicação com o cliente: ${e.message}")
+            } finally {
+                // liberar recursos
+                clientSocket.close()
+                println("Conexão encerrada")
+            }
+        }
     }
 }
